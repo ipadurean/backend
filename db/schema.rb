078a7bb58266382d61f894d33bd88a3d075913b8
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-08-20 09:23:07 -0500

    add link attribute to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_224908) do
+ActiveRecord::Schema.define(version: 2019_08_20_142135) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "link"
   end
 
   create_table "comments", force: :cascade do |t|
